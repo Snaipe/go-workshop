df2e27a0bee9079b6417de1f468bc7c4689caa64
Franklin "Snaipe" Mathieu <snaipe@arista.com>
2026-05-19 14:32:21 +0200

    td0: added solution

--- 0-strings/strings.go
+++ 0-strings/strings.go
@@ -10,13 +10,74 @@ func IndexByte(s string, c byte) int {
 }
 
 func Index(s, substr string) int {
-	panic("unimplemented")
+	if substr == "" {
+		return 0
+	}
+	total := 0
+	for len(s) > 0 {
+		i := IndexByte(s, substr[0])
+		if i == -1 {
+			break
+		}
+		s = s[i:]
+		total += i
+
+		l := min(len(s), len(substr))
+		if s[:l] == substr {
+			return total
+		}
+
+		s = s[1:]
+		total += 1
+	}
+	return -1
 }
 
 func Cut(s, sep string) (before, after string, found bool) {
-	panic("unimplemented")
+	i := Index(s, sep)
+	if i == -1 {
+		return s, "", false
+	}
+	return s[:i], s[i+len(sep):], true
 }
 
 func Split(s, sep string) []string {
-	panic("unimplemented")
+	var result []string
+
+	if sep == "" {
+		for i := 0; i < len(s); i++ {
+			result = append(result, s[i:i+1])
+		}
+		return result
+	}
+
+	for {
+		before, after, found := Cut(s, sep)
+		if !found {
+			result = append(result, before)
+			break
+		}
+
+		result = append(result, before)
+		s = after
+	}
+	return result
+}
+
+func IndexRune(s string, r rune) int {
+	for i, sr := range s {
+		if sr == r {
+			return i
+		}
+	}
+	return -1
+}
+
+func IndexAny(s, chars string) int {
+	for i, r := range s {
+		if IndexRune(chars, r) != -1 {
+			return i
+		}
+	}
+	return -1
 }
--- 0-strings/strings_test.go
+++ 0-strings/strings_test.go
@@ -46,13 +46,239 @@ func TestIndexByte(t *testing.T) {
 }
 
 func TestIndex(t *testing.T) {
-	panic("unimplemented")
+
+	type TestCase struct {
+		Input  string
+		Substr string
+		Index  int
+	}
+
+	cases := []TestCase{
+		{
+			Input:  "Bonjour",
+			Substr: "",
+			Index:  0,
+		},
+		{
+			Input:  "Bonjour",
+			Substr: "jour",
+			Index:  3,
+		},
+		{
+			Input:  "Bonjour",
+			Substr: "joux",
+			Index:  -1,
+		},
+		{
+			Input:  "Hello, 世界",
+			Substr: "世",
+			Index:  7,
+		},
+		{
+			Input:  "Bonjour",
+			Substr: "ou",
+			Index:  4,
+		},
+	}
+
+	for _, tc := range cases {
+		t.Run(tc.Input, func(t *testing.T) {
+			got := Index(tc.Input, tc.Substr)
+			if got != tc.Index {
+				t.Fatalf("wanted %d, got %d", tc.Index, got)
+			}
+		})
+	}
 }
 
 func TestCut(t *testing.T) {
-	panic("unimplemented")
+
+	type TestCase struct {
+		Input  string
+		Sep    string
+		Before string
+		After  string
+		Found  bool
+	}
+
+	cases := []TestCase{
+		{
+			Input:  "Bonjour",
+			Sep:    "",
+			Before: "",
+			After:  "Bonjour",
+			Found:  true,
+		},
+		{
+			Input:  "Bonjour",
+			Sep:    "ou",
+			Before: "Bonj",
+			After:  "r",
+			Found:  true,
+		},
+		{
+			Input:  "a::b:",
+			Sep:    ":",
+			Before: "a",
+			After:  ":b:",
+			Found:  true,
+		},
+		{
+			Input:  "a::b:",
+			Sep:    "x",
+			Before: "a::b:",
+			After:  "",
+			Found:  false,
+		},
+	}
+
+	for _, tc := range cases {
+		t.Run(tc.Input, func(t *testing.T) {
+			before, after, found := Cut(tc.Input, tc.Sep)
+			if before != tc.Before {
+				t.Fatalf("before: wanted %v, got %v", tc.Before, before)
+			}
+			if after != tc.After {
+				t.Fatalf("after: wanted %v, got %v", tc.After, after)
+			}
+			if found != tc.Found {
+				t.Fatalf("found: wanted %v, got %v", tc.Found, found)
+			}
+		})
+	}
+}
+
+func slicesEqual(a, b []string) bool {
+	if len(a) != len(b) {
+		return false
+	}
+	for i := range a {
+		if a[i] != b[i] {
+			return false
+		}
+	}
+	return true
 }
 
 func TestSplit(t *testing.T) {
-	panic("unimplemented")
+
+	type TestCase struct {
+		Input string
+		Sep   string
+		Want  []string
+	}
+
+	cases := []TestCase{
+		{
+			Input: "a::b:",
+			Sep:   ":",
+			Want:  []string{"a", "", "b", ""},
+		},
+		{
+			Input: "a::b:",
+			Sep:   "x",
+			Want:  []string{"a::b:"},
+		},
+		{
+			Input: "a::b:",
+			Sep:   "",
+			Want:  []string{"a", ":", ":", "b", ":"},
+		},
+		{
+			Input: "",
+			Sep:   "",
+			Want:  []string{},
+		},
+	}
+
+	for _, tc := range cases {
+		t.Run(tc.Input, func(t *testing.T) {
+			got := Split(tc.Input, tc.Sep)
+			if !slicesEqual(got, tc.Want) {
+				t.Fatalf("wanted %#v, got %#v", tc.Want, got)
+			}
+		})
+	}
+}
+
+func TestIndexRune(t *testing.T) {
+
+	type TestCase struct {
+		Input string
+		Rune  rune
+		Index int
+	}
+
+	cases := []TestCase{
+		{
+			Input: "",
+			Rune: 0,
+			Index: -1,
+		},
+		{
+			Input: "bonjour",
+			Rune: 'j',
+			Index: 3,
+		},
+		{
+			Input: "bonjour",
+			Rune: 'x',
+			Index: -1,
+		},
+		{
+			Input: "Hello, 世界",
+			Rune: '界',
+			Index: 10,
+		},
+	}
+
+	for _, tc := range cases {
+		t.Run(tc.Input, func(t *testing.T) {
+			got := IndexRune(tc.Input, tc.Rune)
+			if got != tc.Index {
+				t.Fatalf("wanted %d, got %d", tc.Index, got)
+			}
+		})
+	}
+}
+
+func TestIndexAny(t *testing.T) {
+
+	type TestCase struct {
+		Input string
+		Chars string
+		Index int
+	}
+
+	cases := []TestCase{
+		{
+			Input: "abc123",
+			Chars: "1234567890",
+			Index: 3,
+		},
+		{
+			Input: "bonjour",
+			Chars: "xr",
+			Index: 6,
+		},
+		{
+			Input: "bonjour",
+			Chars: "",
+			Index: -1,
+		},
+		{
+			Input: "Hello, 世界, world",
+			Chars: "界世",
+			Index: 7,
+		},
+	}
+
+	for _, tc := range cases {
+		t.Run(tc.Input, func(t *testing.T) {
+			got := IndexAny(tc.Input, tc.Chars)
+			if got != tc.Index {
+				t.Fatalf("wanted %d, got %d", tc.Index, got)
+			}
+		})
+	}
 }
